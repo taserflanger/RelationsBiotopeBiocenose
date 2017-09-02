1ea2fcb5be9b6e515b86c334e8744dbfebc52ab1
Pascal <pascal.nardi@gmail.com>
2017-09-02 17:49:49 +0200

    setup debug and debbuged grid.getTextureOpacities

--- Templates/grid.js
+++ Templates/grid.js
@@ -5,9 +5,9 @@ class Grid {
         this.case_size = 20
         // Size in pixels of each case
         this.rectanglesOrigins = []
-        setRects()
+        this.setRects()
         // Creates Rectangles Origins => Array of coordinates [[x, y], ...]
-        this.propsList = new Array(this.size**2)
+        this.propsList = new Array(Math.pow(this.size, 2))
         for (let i = 0; i < this.propsList.length; i++) {
             this.propsList[i] = {"plant":0, "salinity": Math.random()*100, "water": Math.random()*100, "sunshine": Math.random()*100}
         }
@@ -23,21 +23,22 @@ class Grid {
     }
 
     setRects() {
-        for (x=0; x<this.size; x++) {
+        for (let x=0; x<this.size; x++) {
             for (var y = 0; y < this.size; y++) {
-                this.rectangles.push([x*case_size, y*case_size])
+                this.rectanglesOrigins.push([x*this.case_size, y*this.case_size])
             }
         }
     }
     softmax(vals) {
+        let sensiblility = 1
         let exps = new Array(vals.length)
         let sum = 0
         for (let val in vals) {
-            let e = Math.exp(vals[val]/sensor.value())
+            let e = Math.exp(vals[val]/sensiblility)
             exps[val] = e
             sum += e
         }
-        for (val in exps) {
+        for (let val in exps) {
             exps[val]/=sum
         }
         return exps
@@ -52,28 +53,29 @@ class Grid {
             })
         }
         // Makes operations between two arrays
-        let cases = new Array(this.size**2)
+        let cases = new Array(Math.pow(this.size, 2))
         // array to return. Each element will be the opacity of the corresponding textures
         let ranges = []
         for (let r in this.texturesRanges) {
             ranges.push(this.texturesRanges[r][1]-this.texturesRanges[r][0])
         }
         // array of prop ranges
-        for (let c in cases) {
+        for (let c=0; c<cases.length;c++) {
             let values = new Array(this.textures.length)
+            let i=0
             for (let p in this.propsList[c]) {
-                values[p] = this.propsList[c][p]
+                values[i] = this.propsList[c][p]
+                i++
             }
             // Get the values of prop
-            let vals = operateArrays(ranges, values, (a, b)=> {
-                return a * b
+            let vals = operateArrays(values, ranges, (a, b)=> {
+                return a / b
             })
             // Get the ranges*values
-            let exps = softmax(vals)
+            let exps = this.softmax(vals)
             // Make that all the opacities sum up to 1
-            console.log(exps)
             
-            cases.push(exps)
+            cases[c] = exps
         }
         return cases
     }
@@ -91,3 +93,6 @@ class Grid {
         }
     }
 }
+
+let g = new Grid(10)
+g.getTextureOpacities()
